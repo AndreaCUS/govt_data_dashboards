36d190eb87b73e6e54e4f53382404dd6386df52c
aurrutia <andrea.urrutia@bunsan.io>
2022-12-02 17:18:22 -0600

    add aggregate data file (price by lat and longitude)

--- gas_prices_in_Mexico/clean_data.py
+++ gas_prices_in_Mexico/clean_data.py
@@ -16,6 +16,19 @@ def clean_data():
 
         df["regular"] = pd.to_numeric(df["regular"])
         df["premium"] = pd.to_numeric(df["premium"])
+        df["longitude_int"] = df["longitude"].astype(int)
+        df["latitude_int"] = df["latitude"].astype(int)
         df = df[(df["regular"] > 0) & (df["premium"] > 0)]  # remove 0 price values
-
         df.to_csv("clean_data.csv")
+
+        aggr_df = df
+        aggr_df = aggr_df[
+            (aggr_df["longitude_int"] > 0) & (aggr_df["latitude_int"] > 0)
+        ]
+        grouped_multiple = aggr_df.groupby(["latitude_int", "longitude_int"]).agg(
+            {"regular": ["mean"]}
+        )
+        grouped_multiple.columns = ["regular_mean"]
+        grouped_multiple = grouped_multiple.reset_index()
+
+        grouped_multiple.to_csv("aggregate_data.csv")
