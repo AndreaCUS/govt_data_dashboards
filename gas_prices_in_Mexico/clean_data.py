35bf523ded0cddb3f1ffc1dd6e0867489dcf5b48
aurrutia <andrea.urrutia@bunsan.io>
2022-12-05 17:59:44 -0600

    add postal code / state data

--- gas_prices_in_Mexico/clean_data.py
+++ gas_prices_in_Mexico/clean_data.py
@@ -3,15 +3,16 @@ import pandas as pd
 
 
 def read_data():
-    if not os.path.exists("clean_data.csv"):
+    if os.path.exists("raw_data.csv"):
         df = pd.read_csv("raw_data.csv")
-        return df
+        postal_codes = pd.read_csv("postal_codes.csv")
+        return df, postal_codes
     else:
-        return None
+        return None, None
 
 
 def clean_data():
-    df = read_data()
+    df, postal_codes = read_data()
     if df is not None:
 
         df["regular"] = pd.to_numeric(df["regular"])
@@ -19,16 +20,55 @@ def clean_data():
         df["longitude_int"] = df["longitude"].astype(int)
         df["latitude_int"] = df["latitude"].astype(int)
         df = df[(df["regular"] > 0) & (df["premium"] > 0)]  # remove 0 price values
+
+        # Add state name based on postal code
+        postal_codes.rename(columns={"d_codigo": "codigopostal"}, inplace=True)
+        postal_codes["codigopostal"] = pd.to_numeric(
+            postal_codes["codigopostal"], errors="coerce"
+        )
+        df["codigopostal"] = pd.to_numeric(df["codigopostal"], errors="coerce")
+        df = df.drop_duplicates(subset=["_id"])
+        codigopostal_estado = postal_codes[
+            ["codigopostal", "d_estado"]
+        ].drop_duplicates(subset=["codigopostal"])
+        df = df.merge(codigopostal_estado, on=["codigopostal"])
+
         df.to_csv("clean_data.csv")
 
+        # aggregate data by latitude and longitue, for heatmap
         aggr_df = df
-        aggr_df = aggr_df[
-            (aggr_df["longitude_int"] > 0) & (aggr_df["latitude_int"] > 0)
-        ]
-        grouped_multiple = aggr_df.groupby(["latitude_int", "longitude_int"]).agg(
+        aggr_df = df[
+            (df["longitude_int"] != 0) & (df["latitude_int"] != 0)
+        ]  # remove missing lat & longitude rows
+        aggregate_data = aggr_df.groupby(["latitude_int", "longitude_int"]).agg(
             {"regular": ["mean"]}
         )
-        grouped_multiple.columns = ["regular_mean"]
-        grouped_multiple = grouped_multiple.reset_index()
+        aggregate_data.columns = ["regular_mean"]
+        aggregate_data = aggregate_data.reset_index()
 
-        grouped_multiple.to_csv("aggregate_data.csv")
+        aggregate_data.to_csv("aggregate_data.csv")
+
+        # Summarize data
+        summary_df = pd.DataFrame()
+        summary_df.at[0, "n_rows"] = len(df.index)
+        summary_df.at[0, "min_regular"] = df["regular"].min()
+        summary_df.at[0, "max_regular"] = df["regular"].max()
+        summary_df.at[0, "min_premium"] = df["premium"].min()
+        summary_df.at[0, "max_premium"] = df["premium"].max()
+
+        # Summarize data by state
+        state_data = df.groupby(["d_estado"]).agg(
+            {"regular": ["count", "mean"], "premium": ["count", "mean"]}
+        )
+        state_data.columns = [
+            "regular_count",
+            "regular_mean",
+            "premium_count",
+            "premium_mean",
+        ]
+        state_data = state_data.reset_index()
+        state_data.to_csv("state_data.csv")
+
+
+if __name__ == "__main__":
+    clean_data()
--- gas_prices_in_Mexico/get_data.py
+++ gas_prices_in_Mexico/get_data.py
@@ -1,25 +1,41 @@
 import os
+import io
 import requests
 import pandas as pd
 
 
-def get_data(n_results):
+def get_gas_data(n_results):
     url = "https://api.datos.gob.mx/v1/precio.gasolina.publico?pageSize={}".format(
         n_results
     )
     raw_data = requests.get(url)
     json_data = raw_data.json()
-
-    return json_data
-
-
-def save_data(json_data, n_results):
     df = pd.DataFrame(json_data["results"], index=list(range(0, n_results)))
-    df.to_csv("raw_data.csv")
-    pass
+
+    return df
+
+
+def get_postal_code_data():
+    url = "https://www.correosdemexico.gob.mx/datosabiertos/cp/cpdescarga.txt"
+    urlData = requests.get(url)
+    decoded = urlData.content.decode("latin-1")
+    decoded = "\n".join(decoded.split("\n")[1:])  # remove 1st row, which is not data
+    df = pd.read_csv(io.StringIO(decoded), sep="|")
+
+    # df = pd.read_csv(urlData, encoding='latin1')
+    # json_data = raw_data.json()
+    # df = pd.DataFrame(json_data)
+    return df
 
 
 def make_data_file(n_results=5000):
     if not os.path.isfile("raw_data.csv"):
-        json_data = get_data(n_results)
-        save_data(json_data, n_results)
+        df = get_data(n_results)
+        df.to_csv("raw_data.csv")
+    if not os.path.isfile("postal_codes.csv"):
+        df = get_postal_code_data()
+        df.to_csv("postal_codes.csv")
+
+
+if __name__ == "__main__":
+    make_data_file()
